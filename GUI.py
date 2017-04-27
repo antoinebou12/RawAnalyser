f2097f0f68f4ea51ed0b6d5d25e6aeed750ce912
Antoine Boucher <antoine.bou13@gmail.com>
2017-04-27 11:19:35 -0400

    RawAnalyser
    Simple RawAnalyser for testing black and white level of clipping in a
    raw image.Image selection for the region of interest in a raw image
    Camera: OV2740
    Resolution: (1088, 1928)
    ImageType.BINARY16U
    Sensor value: 10 bit
    Bayer pattern: bggr

---
+++ GUI.py
@@ -0,0 +1,112 @@
+from PIL import Image
+import numpy as np
+import time
+import sys, os, random, json
+from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication, QWidget, QFileDialog, QLineEdit, QPushButton, QLabel, QColumnView, QFileSystemModel,QSplitter, QTreeView, QListView
+from PyQt5.QtGui import QIcon,QPixmap
+from PyQt5.QtCore import QDir, Qt
+from win32api import GetSystemMetrics
+from PIL.ImageQt import ImageQt
+from scipy.misc import imsave
+import PIL.Image as Image
+
+
+widthScreen = GetSystemMetrics(0)
+heightScreen = GetSystemMetrics(1)
+widthApp = widthScreen
+heightApp = heightScreen
+
+
+
+
+class MenuFile(QMainWindow):
+    def __init__(self, parent=None):
+        super(MenuFile,self).__init__(parent)
+        self.setCentralWidget(FormWidget(self))
+        self.Menu()
+
+
+    def Menu(self):
+        openFile = QAction('Open', self)
+        openFile.setShortcut('Ctrl+O')
+        openFile.setStatusTip('Open new File')
+        openFile.triggered.connect(self.showDialog)
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(openFile)
+
+        self.resize(widthApp, heightApp)
+        self.move(0, 0)
+        self.setWindowTitle('RawView')
+        self.setWindowIcon(QIcon('image\icon.png'))
+
+    def showDialog(self):
+        self.setCentralWidget(FormWidget(self))
+
+
+class FormWidget(QWidget):
+
+    def __init__(self, parent):
+        super(FormWidget, self).__init__(parent)
+        self.UI()
+    def UI(self):
+
+        self.fname = QFileDialog.getOpenFileName(self, 'Open file', 'C:/Users/antoine.boucher/Desktop/', "(*.tiff *.raw)")
+        print self.fname[0]
+        name = self.fname[0]
+
+
+        if name[-4:]=='tiff':
+            self.pix = QPixmap(self.fname[0])
+        elif name[-3:]=='raw':
+            f = open(self.fname[0], "rb")
+            image_array = np.fromfile(f, dtype=np.uint16, count=1928 * 1088)
+            image = Image.frombuffer("I", [1928, 1088], image_array.astype('I'), 'raw', 'I', 0, 1)
+            imsave('tiff/raw.tiff', image)
+            time.sleep(1)
+            self.pix = QPixmap('tiff/raw.tiff')
+
+
+
+        self.image = QLabel(self)
+        self.image.setPixmap(self.pix)
+        self.image.setObjectName("image")
+        self.image.mousePressEvent = self.getPos
+        self.num =0
+        self.feeds = {
+            'first_x': 1,
+            'first_y': 1,
+            'last_x' : 1928,
+            'last_y' : 1088
+        }
+
+    def getPos(self, event):
+        x = event.pos().x()
+        y = event.pos().y()
+        print x
+        print y
+        self.num=self.num+1
+        with open('json/points.json','w') as f:
+            if self.num % 2 == 1:
+                self.feeds['first_x'] = x
+                self.feeds['first_y'] = y
+            elif self.num % 2 == 0:
+                self.feeds['last_x'] = x
+                self.feeds['last_y'] = y
+
+            json.dump(self.feeds, f)
+
+def main():
+    app = QApplication([])
+    ex = MenuFile()
+    ex.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
---
+++ RawAnalyser.py
@@ -0,0 +1,174 @@
+import sys,os, getopt, json, winsound, time
+import numpy as np
+from scipy.misc import imsave
+from scipy import ndimage
+import PIL.Image as Image
+from PIL import *
+import plotly
+plotly.tools.set_credentials_file(username='antoine13', api_key='DZN51FyyaBILK1Ie1InJ')
+import plotly.plotly as py
+import plotly.graph_objs as go
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+global numFile
+numFile=0
+global pointfile
+pointfile = 'json/points.json'
+
+
+
+class NewAnalyse(FileSystemEventHandler):
+    def on_created(self, event):
+        global numFile
+        global newFile
+        newFile = event.src_path
+        print event.src_path
+        numFile=numFile+1
+        with open(pointfile, 'r') as f:
+            point = json.load(f)
+
+        f = open(newFile, "rb")
+
+
+        # list in text file
+        #thefileCrop = open('array/image_crop.txt', 'w')
+        # thefileArray = open('array/image_array.txt', 'w')
+        # thefileMatrix = open('array/matrix.txt', 'w')
+        # thefileChannelG = open('array/G.txt', 'w')
+        # thefileChannelB = open('array/B.txt', 'w')
+        # thefileChannelR = open('array/R.txt', 'w')
+
+        image_array = np.fromfile(f, dtype=np.uint16, count=1928 * 1088)
+        # for item in image_array:
+        #     thefileArray.write("%s\n" % item)
+        # raw to tiff
+        image = Image.frombuffer("I", [1928, 1088], image_array.astype('I'), 'raw', 'I', 0, 1)
+        imsave('tiff/' + str(numFile) + '.tiff', image)
+        #Matrix 2d of the image
+        cols_count, rows_count = 1928, 1088;
+
+        matrix = [[0 for x in range(cols_count)] for y in range(rows_count)]
+        for i in range(1, 1929):
+            for j in range(1, 1089):
+                matrix[j - 1][i - 1] = image_array[(i * j) - 1]
+                # Dead Pixel
+                #if matrix[j-1][i-1] == 0:
+                    # print 'deadPixel ' + 'x: ' + str((i-1)) + ' y: ' + str((j-1))
+        #Write in text file the list
+        # for item in matrix:
+        #     thefileMatrix.write("%s\n" % item)
+
+
+
+        cropMatrix = [[0 for x in range(cols_count)] for y in range(rows_count)]
+        for xCrop in range(point['first_x'], point['last_x'], 1):
+            for yCrop in range(point['first_y'], point['last_y'], 1):
+                cropMatrix[yCrop][xCrop] = matrix[yCrop][xCrop]
+        # Write in text file the list
+        # for itemCrop in cropMatrix:
+        #    thefileCrop.write("%s\n" % itemCrop)
+
+        ChannelB = [[0 for x in range(cols_count)] for y in range(rows_count)]  # 524416
+        ChannelR = [[0 for x in range(cols_count)] for y in range(rows_count)]  # 524416
+        ChannelG = [[0 for x in range(cols_count)] for y in range(rows_count)]  # 1048832
+
+        for i in range(0, 1928, 2):
+            for j in range(0, 1028, 2):
+                ChannelB[j][i] = cropMatrix[j][i]
+
+        for i in range(1, 1928, 2):
+            for j in range(0, 1028, 2):
+                ChannelG[j][i] = cropMatrix[j][i]
+
+        for i in range(0, 1928, 2):
+            for j in range(1, 1028, 2):
+                ChannelG[j][i] = cropMatrix[j][i]
+
+        for i in range(1, 1928, 2):
+            for j in range(1, 1028, 2):
+                ChannelR[j][i] = cropMatrix[j][i]
+        # Write in text file the list
+        # for item in ChannelB:
+        #     thefileChannelB.write("%s\n" % item)
+        # for item in ChannelG:
+        #     thefileChannelG.write("%s\n" % item)
+        # for item in ChannelR:
+        #     thefileChannelR.write("%s\n" % item)
+
+
+        print 'Clipping at 1 (10bit) for black level'
+        print 'R Black level ' + str(min(np.array(ChannelB)[np.nonzero(np.array(ChannelB))])/64)
+        print 'G Black level ' + str(min(np.array(ChannelG)[np.nonzero(np.array(ChannelG))])/64)
+        print 'B Black level ' + str(min(np.array(ChannelR)[np.nonzero(np.array(ChannelR))])/64)
+
+        if ((min(np.array(ChannelB)[np.nonzero(np.array(ChannelB))])<=64) or (min(np.array(ChannelG)[np.nonzero(np.array(ChannelG))])<=64) or (min(np.array(ChannelR)[np.nonzero(np.array(ChannelR))])<=64)):
+        # winsound.PlaySound('sound/black.wav', winsound.SND_FILENAME)
+          print 'CLIPPING BLACK'
+        else:
+          print 'NOT CLIPPING BLACK'
+
+
+
+        print 'Clipping at 1023 (10bit) for white level'
+        print 'R White level ' + str(np.array(ChannelR).max()/64)
+        print 'G White level ' + str(np.array(ChannelG).max()/64)
+        print 'B White level ' + str(np.array(ChannelB).max()/64)
+
+        if ((np.array(ChannelR).max()>= 65472) or (np.array(ChannelR).max()>=65472) or (np.array(ChannelR).max()>=65472)):
+        #  winsound.PlaySound('sound/white.wav', winsound.SND_FILENAME)
+           print 'CLIPPING WHITE'
+        else:
+           print 'NOT CLIPPING WHITE'
+
+
+
+
+
+
+
+
+
+def main(argv):
+    global newFile
+    inputfolder = 'C:/Users/antoine.boucher/Desktop/Algolux/raw'
+    pointfile = 'json/points.json'
+    try:
+        opts, args = getopt.getopt(argv, "hf:p:", ["ffile=","pfile"])
+    except getopt.GetoptError:
+        print 'test.py -f <pathfolder> -p <pointfile> '
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'The program will check for new file and analysed them'
+            print '-f <pathfolder> raw image folder'
+            print '-p <pointfile> of the region of interest'
+            sys.exit()
+        elif opt in ("-f", "--ffile"):
+            inputfolder = arg
+        elif opt in ("-p", "--pfile"):
+            jsonfile = arg
+    print 'Folder:', inputfolder
+    print 'Json:', pointfile
+
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    event_handler = NewAnalyse()
+    observer = Observer()
+    observer.schedule(event_handler, inputfolder, recursive=False)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
+
+
+
+
