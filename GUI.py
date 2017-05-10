801dca7bf65e22dfefeb1e6b8f32ecabbc3f3e50
Antoine Boucher <antoine.bou13@gmail.com>
2017-05-10 10:49:33 -0400

    best region of interest selector

--- GUI.py
+++ GUI.py
@@ -1,20 +1,16 @@
-
 import sys, os, random, json
 import time
 from win32api import GetSystemMetrics
-from os.path import basename 
+from os.path import basename
 import tifffile as tiff
 
 import numpy as np
 from PIL.ImageQt import ImageQt
 
-from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication, QWidget, QFileDialog, QLineEdit, QPushButton, QLabel, QColumnView, QFileSystemModel,QSplitter, QTreeView, QListView
-from PyQt5.QtGui import QIcon,QPixmap
-from PyQt5.QtCore import QDir, Qt
-
-
-
-
+from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication, QWidget, QFileDialog, QLineEdit, QPushButton, \
+    QLabel, QColumnView, QFileSystemModel, QSplitter, QTreeView, QListView
+from PyQt5.QtGui import QIcon, QPixmap,QMouseEvent
+from PyQt5.QtCore import QDir, Qt, QEvent
 
 widthScreen = GetSystemMetrics(0)
 heightScreen = GetSystemMetrics(1)
@@ -22,14 +18,11 @@ widthApp = widthScreen
 heightApp = heightScreen
 
 
-
-
 class MenuFile(QMainWindow):
     def __init__(self, parent=None):
-        super(MenuFile,self).__init__(parent)
+        super(MenuFile, self).__init__(parent)
         self.setCentralWidget(FormWidget(self))
         self.Menu()
-
 
     def Menu(self):
         openFile = QAction('Open', self)
@@ -51,40 +44,45 @@ class MenuFile(QMainWindow):
 
 
 class FormWidget(QWidget):
-
     def __init__(self, parent):
         super(FormWidget, self).__init__(parent)
+        qApp.installEventFilter(self)
         self.UI()
-    def UI(self):
 
-        self.fname = QFileDialog.getOpenFileName(self, 'Open file', 'C:/Users/antoine.boucher/Desktop/', "(*.tiff *.raw)")
+    def UI(self):
+        global position
+        self.fname = QFileDialog.getOpenFileName(self, 'Open file', 'C:/Users/antoine.boucher/Desktop/',
+                                                 "(*.tiff *.raw)")
         print self.fname[0]
         name = self.fname[0]
-
-
-        if name[-4:]=='tiff':
+        global matrix
+        cols_count = 1928
+        rows_count = 1088
+        matrix = [[0 for x in range(cols_count)] for y in range(rows_count)]
+        if name[-4:] == 'tiff':
             self.pix = QPixmap(self.fname[0])
-        elif name[-3:]=='raw':
+            image_array = np.fromfile(self.fname[0], dtype=np.uint16, count=1928 * 1088)
+            matrix = np.array(matrix)
+            matrix = np.reshape(image_array, (1088, 1928))
+        elif name[-3:] == 'raw':
             f = open(self.fname[0], "rb")
             image_array = np.fromfile(f, dtype=np.uint16, count=1928 * 1088)
-            
             matrix = [[0 for x in range(cols_count)] for y in range(rows_count)]
             matrix = np.array(matrix)
             matrix = np.reshape(image_array, (1088, 1928))
             # raw to tiff
-            tiff.imsave('tiff/'+ basename(name) +'raw.tiff', matrix)
+            tiff.imsave('tiff/' + basename(name) + 'raw.tiff', matrix)
             time.sleep(1)
-            self.pix = QPixmap('tiff/'+ basename(name) +'raw.tiff')
-            self.text = 'first ROI'
-            self.setToolTip(self.text)
-
-
+            self.pix = QPixmap('tiff/' + basename(name) + 'raw.tiff')
 
         self.image = QLabel(self)
         self.image.setPixmap(self.pix)
         self.image.setObjectName("image")
         self.image.mousePressEvent = self.getPos
-        self.num =0
+        self.num = 0
+        self.point = 'first ROI'
+        self.text = ''
+        self.setToolTip(self.text)
         self.feeds = {
             'gray_first_x': 1,
             'gray_first_y': 1,
@@ -92,38 +90,62 @@ class FormWidget(QWidget):
             'first_y': 1,
             'last_x': 1928,
             'last_y': 1088,
-            'gray_last_x':1,
-            'gray_last_y':1
+            'gray_last_x': 1,
+            'gray_last_y': 1
         }
+    def eventFilter(self, source, event):
+        if event.type() == QEvent.MouseMove:
+            if event.buttons() == Qt.NoButton:
+                global position
+                position = event.pos()
+                print ''
+                print position.x()
+                print position.y()
+                print ''
+                self.text = self.point  + 'x: ' + str(position.x()) + ' y:' + str(position.y()) + ' value ' + str(
+                    matrix[position.y()][position.x()] / 64)
+                self.setToolTip(self.text)
+        else:
+            pass  # do other stuff
+        return super(FormWidget, self).eventFilter(source, event)
 
     def getPos(self, event):
-        x = event.pos().x()
-        y = event.pos().y()
+        global matrix
+        mouse_x = event.pos().x()
+        mouse_y = event.pos().y()
         print x
         print y
-        self.num=self.num+1
-        with open('json/points.json','w') as f:
+        self.num = self.num + 1
+        with open('json/points.json', 'w') as f:
             if self.num % 4 == 1:
                 self.feeds['first_x'] = x
                 self.feeds['first_y'] = y
-                self.text = 'last ROI'
+                self.point = 'last ROI '
+                self.text = self.point + 'x: ' + str(position.x()) + ' y:' + str(position.y()) + ' value ' + str(matrix[position.y()][position.x()]/64)
                 self.setToolTip(self.text)
             elif self.num % 4 == 2:
                 self.feeds['last_x'] = x
                 self.feeds['last_y'] = y
-                self.text = 'first Gray'
+                self.point = 'first gray '
+                self.text = self.point + 'x: ' + str(position.x()) + ' y:' + str(position.y()) + ' value ' + str(
+                    matrix[position.y()][position.x()] / 64)
                 self.setToolTip(self.text)
             elif self.num % 4 == 3:
                 self.feeds['gray_first_x'] = x
                 self.feeds['gray_first_y'] = y
-                self.text = 'last Gray'
+                self.point = 'last gray '
+                self.text = self.point + 'x: ' + str(position.x()) + ' y:' + str(position.y()) + ' value ' + str(
+                    matrix[position.y()][position.x()] / 64)
                 self.setToolTip(self.text)
             elif self.num % 4 == 0:
                 self.feeds['gray_last_x'] = x
                 self.feeds['gray_last_y'] = y
-                self.text = 'first ROI'
+                self.point = 'first ROI'
+                self.text = self.point + 'x: ' + str(position.x()) + ' y:' + str(position.y()) + ' value ' + str(
+                    matrix[position.y()][position.x()] / 64)
                 self.setToolTip(self.text)
             json.dump(self.feeds, f)
+
 
 def main():
     app = QApplication([])
@@ -134,7 +156,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
 
